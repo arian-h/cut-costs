de75508c02faacc5936fa83ced4b4ea6af7854b4
arian-h <aryan.hosseinzadeh@gmail.com>
2017-11-19 01:50:27 -0800

    Fixed database connection configuration and Turned off SSL

--- src/main/java/com/boot/cut_costs/config/datasource/PersistenceConfiguration.java
+++ src/main/java/com/boot/cut_costs/config/datasource/PersistenceConfiguration.java
@@ -1,8 +1,9 @@
 package com.boot.cut_costs.config.datasource;
 
-import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
 
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +14,16 @@ public class PersistenceConfiguration {
 	@Bean
 	@Primary
 	@ConfigurationProperties(prefix="spring.datasource")
-	public DataSource dataSource() {
-		return DataSourceBuilder.create().build();
+	public BasicDataSource dataSource() throws URISyntaxException {
+		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = String.format("jdbc:mysql://%s:%d%s?useSSL=false", dbUri.getHost(), dbUri.getPort(),  dbUri.getPath());
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+        return basicDataSource;
 	}
 }
